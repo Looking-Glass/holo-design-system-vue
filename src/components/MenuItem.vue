b8a4d5e327779b6c42d99767cbfabe590491b981
Mariano Miguel <mariano@mariano.io>
2021-07-12 01:18:11 -0300

    feat: callback functions for login/logout

--- src/components/MenuItem.vue
+++ src/components/MenuItem.vue
@@ -1,5 +1,5 @@
 <template>
-  <component :is="component" class="font-sans-alt inline-block w-full cursor-pointer text-base p-2 bg-white hover:bg-gray-100 focus:bg-gray-100">
+  <component :is="component" class="text-left font-sans-alt inline-block w-full cursor-pointer text-base p-2 bg-white hover:bg-gray-100 focus:bg-gray-100" v-bind="$attrs" @click="emitClick">
     <slot />
   </component>
 </template>
@@ -10,6 +10,11 @@ export default {
     component: {
       type: [Object, String],
       default: 'a'
+    }
+  },
+  methods: {
+    emitClick() {
+      this.$emit('click')
     }
   }
 }
